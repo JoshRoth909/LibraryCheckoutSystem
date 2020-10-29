ffaf8b5f5c25a0c189cf922a160421c12513931c
KristopherLea <kristopherlea187@gmail.com>
2020-10-29 16:45:50 -0500

    created cart model

---
+++ src/main/java/com/labC/libraryCheckoutSystem/cartModel/Cart.java
@@ -0,0 +1,49 @@
+package com.labC.libraryCheckoutSystem.cartModel;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private int id;
+    private String book;
+    private boolean checkStatus;
+
+    public Cart(int id, String book, boolean checkStatus) {
+        this.id = id;
+        this.book = book;
+        this.checkStatus = checkStatus;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getBook(){
+        return book;
+    }
+
+    public void setBook(String book){
+        this.book = book;
+    }
+
+    public boolean getCheckStatus(){
+        return checkStatus;
+    }
+
+    public void setCheckStatus(boolean checkStatus){
+        this.checkStatus = checkStatus;
+    }
+
+}
