5129ee27c696fe832315f3c72869fb8bbcf098ca
User <user@Users-Macintosh.attlocal.net>
2020-10-29 18:02:25 -0400

    first bookshelfModel draft

--- src/main/java/com/labC/libraryCheckoutSystem/Model/Bookshelf.java
+++ src/main/java/com/labC/libraryCheckoutSystem/Model/Bookshelf.java
@@ -1,19 +1,29 @@
 package com.labC.libraryCheckoutSystem.Model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Bookshelf {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private int id;
 
     public Bookshelf() {}
 
-    public Long getId() {
+    public int getId() {
         return id;
+    }
+    @OneToOne(mappedBy = "user_id")
+    private Users users;
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    @OneToOne(mappedBy = "book_id")
+    private Books book;
+
+    public int getBook() {
+        return book_id;
     }
 }
