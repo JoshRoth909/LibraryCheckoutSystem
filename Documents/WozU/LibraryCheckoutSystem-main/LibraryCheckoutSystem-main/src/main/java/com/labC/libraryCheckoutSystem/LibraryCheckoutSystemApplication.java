439e3647cacc8dc2a6d9b3df53f3a8558dfc0496
unknown <meicy101@gmail.com>
2020-11-02 16:56:41 -0600

    Second

---
+++ Documents/WozU/LibraryCheckoutSystem-main/LibraryCheckoutSystem-main/src/main/java/com/labC/libraryCheckoutSystem/LibraryCheckoutSystemApplication.java
@@ -0,0 +1,13 @@
+package com.labC.libraryCheckoutSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCheckoutSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryCheckoutSystemApplication.class, args);
+	}
+
+}
