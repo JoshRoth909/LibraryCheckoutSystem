a3a76e9ae96a3f1101db458221cfcafd78ab3b42
RayRoyal1876 <tenthwonderind@icloud.com>
2020-11-02 16:59:11 -0500

    ChangedToInterface

--- src/main/java/com/labC/libraryCheckoutSystem/BookshelfRepo.java
+++ src/main/java/com/labC/libraryCheckoutSystem/BookshelfRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class BookshelfRepo extends CrudRepository<Bookshelf, Long> {
+public interface BookshelfRepo extends CrudRepository<Bookshelf, Long> {
 }
