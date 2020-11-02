7a7ff51f4bcc98a1749d3c2c0d3959d9616f6e8a
RayRoyal1876 <tenthwonderind@icloud.com>
2020-11-02 15:51:25 -0500

    https://github.com/JoshRoth909/LibraryCheckoutSystem.git

---
+++ src/main/java/com/labC/libraryCheckoutSystem/BookshelfRepo.java
@@ -0,0 +1,7 @@
+package com.labC.libraryCheckoutSystem;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public class BookshelfRepo extends CrudRepository<Bookshelf, Integer> {
+}
--- src/main/java/repo/BookshelfRepo.java
+++
@@ -1,6 +0,0 @@
-package repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public class BookshelfRepo extends JpaRepository<Bookshelf, Integer> {
-}
