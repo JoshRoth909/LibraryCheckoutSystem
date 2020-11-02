c60f11ecc3dee82937836edf7772ba8d7ccecc01
User <user@Users-Macintosh.local>
2020-11-02 17:37:23 -0500

    changed relationship of books to onetoone and separated states and methods for readability

--- src/main/java/Bookshelf.java
+++ src/main/java/Bookshelf.java
@@ -3,24 +3,20 @@ import javax.persistence.*;
 @Entity
 public class Bookshelf {
     @Id
+
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+
+    @OneToOne(mappedBy = "book_id")
+    private Long book;
+
+    @OneToOne(mappedBy = "user_id")
+    private Long id;
+    private Users users;
 
     public Bookshelf() {}
-
     public long getId() {
         return id;
     }
-    @OneToOne(mappedBy = "user_id")
-    private Users users;
-
-    public long getUser_id() {
-        return user_id;
-    }
-
-    @OneToMany(mappedBy = "book_id")
-    private Books book;
-
     public long getBook() {
         return book_id;
     }
