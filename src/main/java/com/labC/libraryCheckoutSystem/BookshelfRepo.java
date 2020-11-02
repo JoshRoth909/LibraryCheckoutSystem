c09ca5d63a271dfa75adbcaac1f1ca298d6820ac
RayRoyal1876 <tenthwonderind@icloud.com>
2020-11-02 16:17:06 -0500

    AddedAnnotation

--- src/main/java/com/labC/libraryCheckoutSystem/BookshelfRepo.java
+++ src/main/java/com/labC/libraryCheckoutSystem/BookshelfRepo.java
@@ -2,6 +2,8 @@ package com.labC.libraryCheckoutSystem;
 
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class BookshelfRepo extends CrudRepository<Bookshelf, Integer> {
 }
