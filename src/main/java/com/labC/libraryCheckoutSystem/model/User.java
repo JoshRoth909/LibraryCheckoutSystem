ae0fe2ddf00a2037aec304570e830c85ac4e43d5
pabloalonso-20 <alonso.pablo20@gmail.com>
2020-11-02 15:13:46 -0600

    changed id data type from int to long

--- src/main/java/com/labC/libraryCheckoutSystem/model/User.java
+++ src/main/java/com/labC/libraryCheckoutSystem/model/User.java
@@ -10,11 +10,11 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private long id;
     private String name;
     private String email;
 
-    public User(int id, String name, String email) {
+    public User(long id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -22,7 +22,7 @@ public class User {
 
     public User(){}
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
