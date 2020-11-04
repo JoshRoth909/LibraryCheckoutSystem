bb11aea48f98379614265fa064ff924fcaf95bee
unknown <meicy101@gmail.com>
2020-11-04 13:47:41 -0600

    fixed @Repo

--- src/main/java/com/labC/libraryCheckoutSystem/BookRepo.java
+++ src/main/java/com/labC/libraryCheckoutSystem/BookRepo.java
@@ -1,7 +1,9 @@
 package com.labC.libraryCheckoutSystem;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepo extends CrudRepository<Book,Long> {
 
 }
