6bf2feadc7e721bd1f873f55f4455d9e6f6fa8bd
Quanda Moore <72464560+quedupmama@users.noreply.github.com>
2020-11-05 17:05:42 -0500

    updated code

--- src/main/java/com/labC/libraryCheckoutSystem/LibraryCheckoutSystemApplication.java
+++ src/main/java/com/labC/libraryCheckoutSystem/LibraryCheckoutSystemApplication.java
@@ -11,4 +11,31 @@ public class LibraryCheckoutSystemApplication {
 	}
 
 }
+ class UserController{
+
+
+	private String name;
+	public String id;
+
+	public UserController(String name, String id){
+		this.name = name;
+		this.id = id;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getId(){
+	return id;
+}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}
 
